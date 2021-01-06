d375cfbb92d4330257435e93fdb053e62e7d3989
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-06 17:25:55 +0330

    add pdf base view

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,10 +1,32 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Report Document</title>
-</head>
-<body>
-    Salam Donya!
-</body>
+	<head>
+		<meta charset="utf-8">
+        <title>{{ $title }}</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        {{--<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">--}}
+    </head>
+	<body>
+		<div class="wrapper">
+		    <div class="pb-5">
+			    <div class="middle pb-10 title">{{ $title }}</div>
+    			<x-laravel-reporter::meta />
+		    </div>
+		    <div class="content">
+		    	<table class="table">
+                    <x-laravel-reporter::table-header :header="$header" :columns="$columns"/>
+                    @foreach($query->cursor() as $row)
+                        <x-laravel-reporter-row :row="$row" :columns="$columns" :is-even="$loop->index % 2 === 0"/>
+		    		@endforeach
+		    	</table>
+			</div>
+		</div>
+	    <?php
+            // todo delete?
+		    if ( isset($pdf) ) {
+		        $pdf->page_text(30, ($pdf->get_height() - 26.89), "Date Printed: " . date('d M Y H:i:s'), null, 10);
+		    	$pdf->page_text(($pdf->get_width() - 84), ($pdf->get_height() - 26.89), "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10);
+		    }
+	    ?>
+	</body>
 </html>
