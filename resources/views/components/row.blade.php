8385507c780906bfa71dbb4ad9cefe11e1fad07f
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-07 13:41:34 +0330

    always have padding on row cells

--- resources/views/components/row.blade.php
+++ resources/views/components/row.blade.php
@@ -1,6 +1,6 @@
 {{-- todo config or something for even/odd classes? --}}
 <tr class="{{ $isEven ? 'bg-gray-100' : '' }}">
     @foreach($formattedRow as $column)
-        <td class="{{ $column->class }}">{{ $column->title }}</td>
+        <td class="px-2 py-1 {{ $column->class }}">{{ $column->title }}</td>
     @endforeach
 </tr>
