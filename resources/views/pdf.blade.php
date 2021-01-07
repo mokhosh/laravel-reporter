4444864249f2e0f0bdf539d02a7f68f4a617cfb5
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-07 14:19:23 +0330

    switch to tailwind 1 as a test

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -3,8 +3,8 @@
 	<head>
 		<meta charset="utf-8">
         <title>{{ $title }}</title>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-        {{--<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">--}}
+        {{--<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">--}}
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     </head>
 	<body>
         <div class="p-12">
@@ -20,12 +20,5 @@
                 </table>
             </div>
         </div>
-	    <?php
-            // todo delete?
-		    if ( isset($pdf) ) {
-		        $pdf->page_text(30, ($pdf->get_height() - 26.89), "Date Printed: " . date('d M Y H:i:s'), null, 10);
-		    	$pdf->page_text(($pdf->get_width() - 84), ($pdf->get_height() - 26.89), "Page {PAGE_NUM} of {PAGE_COUNT}", null, 10);
-		    }
-	    ?>
 	</body>
 </html>
