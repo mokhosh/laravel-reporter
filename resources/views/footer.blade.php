f2725ed8f2fa1f7fbe5e720fee5a05fe82d9807f
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 11:10:25 +0330

    try inline styles

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,7 +1,20 @@
-<style>@import "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";</style>
+<style>
+    .flex {
+        display: flex;
+    }
+    .justify-between {
+        justify-content: space-between;
+    }
+    .p-12 {
+        padding: 4rem;
+    }
+    .text-lg {
+        font-size: 2rem;
+    }
+</style>
 <div class="flex justify-between p-12 text-lg">
     <div class="date"></div>
-    <div>
+    <div class="flex">
         <div class="pageNumber"></div>
         <div>of</div>
         <div class="totalPages"></div>
