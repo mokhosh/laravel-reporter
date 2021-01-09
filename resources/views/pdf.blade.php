fc9d67388dcf529951b7a37b4b631434a448f1d1
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 10:13:18 +0330

    remove margins from html template

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -7,18 +7,16 @@
         <style>html{-webkit-print-color-adjust:exact}</style>
     </head>
 	<body>
-        <div class="p-12">
-            <div class="font-bold text-2xl text-gray-700 text-center font-serif">{{ $title }}</div>
-            <x-laravel-reporter::meta :meta="$meta"/>
+        <div class="font-bold text-2xl text-gray-700 text-center font-serif">{{ $title }}</div>
+        <x-laravel-reporter::meta :meta="$meta"/>
 
-            <div class="mt-4 rounded-lg overflow-hidden border border-gray-100">
-                <table class="w-full">
-                    <x-laravel-reporter::table-header :header="$header" :columns="$columns"/>
-                    @foreach($query->cursor() as $row)
-                        <x-laravel-reporter-row :row="$row" :columns="$columns" :is-even="$loop->index % 2 === 0"/>
-                    @endforeach
-                </table>
-            </div>
+        <div class="mt-4 rounded-lg overflow-hidden border border-gray-100">
+            <table class="w-full">
+                <x-laravel-reporter::table-header :header="$header" :columns="$columns"/>
+                @foreach($query->cursor() as $row)
+                    <x-laravel-reporter-row :row="$row" :columns="$columns" :is-even="$loop->index % 2 === 0"/>
+                @endforeach
+            </table>
         </div>
 	</body>
 </html>
