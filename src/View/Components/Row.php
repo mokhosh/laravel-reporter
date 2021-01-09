97bc7f76d03295ae51d7312eb123142014078232
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 09:44:43 +0330

    fix non string columns throwing exception

--- src/View/Components/Row.php
+++ src/View/Components/Row.php
@@ -2,10 +2,11 @@
 
 namespace Mokhosh\Reporter\View\Components;
 
-use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\View\Component;
+use JetBrains\PhpStorm\Pure;
+use Stringable;
 
 class Row extends Component
 {
@@ -26,11 +27,19 @@ class Row extends Component
                     : '',
                 'title' => is_array($modifier)
                     ? $modifier['transform']($this->row->{$title})
-                    : $this->row->{$title},
+                    : $this->stringify($this->row->{$title}),
             ];
         }
 
         return $columns;
+    }
+
+    #[Pure]
+    public function stringify(mixed $mixed): string
+    {
+        if (is_array($mixed)) return implode($mixed);
+        if (is_object($mixed) && !!! $mixed instanceof Stringable) return '-';
+        return (string) $mixed;
     }
 
     public function render(): View
