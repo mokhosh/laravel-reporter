afba6396c71812a69de55e44afd2275e16c3da6e
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 10:52:53 +0330

    nother shot

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,5 @@
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<style>html{-webkit-print-color-adjust:exact}</style>
 <div class="flex justify-between p-12 text-lg">
     <div class="date"></div>
     <div>
