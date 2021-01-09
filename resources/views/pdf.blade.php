2c0435b54c47a6ea357b67404678d3ae441576d9
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 10:54:34 +0330

    move footer to main template

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -18,5 +18,14 @@
                 @endforeach
             </table>
         </div>
+
+        <div class="flex justify-between p-12 text-lg">
+            <div class="date"></div>
+            <div>
+                <div class="pageNumber"></div>
+                <div>of</div>
+                <div class="totalPages"></div>
+            </div>
+        </div>
 	</body>
 </html>
