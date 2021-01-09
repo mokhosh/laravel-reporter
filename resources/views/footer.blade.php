b73f8f529cec25f18ed029c28a16de9754e2ad06
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 11:03:25 +0330

    move tailwind to a component

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+<x-tailwind/>
+<div class="flex justify-between p-12 text-lg">
+    <div class="date"></div>
+    <div>
+        <div class="pageNumber"></div>
+        <div>of</div>
+        <div class="totalPages"></div>
+    </div>
+</div>
--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
         <title>{{ $title }}</title>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <x-tailwind/>
         <style>html{-webkit-print-color-adjust:exact}</style>
     </head>
 	<body>
@@ -17,15 +17,6 @@
                     <x-laravel-reporter-row :row="$row" :columns="$columns" :is-even="$loop->index % 2 === 0"/>
                 @endforeach
             </table>
-        </div>
-
-        <div class="flex justify-between p-12 text-lg">
-            <div class="date"></div>
-            <div>
-                <div class="pageNumber"></div>
-                <div>of</div>
-                <div class="totalPages"></div>
-            </div>
         </div>
 	</body>
 </html>
