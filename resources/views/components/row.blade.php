494d3cf965ab0216227fc4473eed88c488b33cd3
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 10:23:25 +0330

    try lower bg opacity to make alternating bg's visible

--- resources/views/components/row.blade.php
+++ resources/views/components/row.blade.php
@@ -1,6 +1,6 @@
 {{-- todo config or something for even/odd classes? --}}
 <tr class="{{ $isEven ? 'bg-gray-100' : '' }}">
     @foreach($formattedRow as $column)
-        <td class="px-2 py-1 bg-opacity-70 {{ $column->class }}">{{ $column->title }}</td>
+        <td class="px-2 py-1 bg-opacity-50 {{ $column->class }}">{{ $column->title }}</td>
     @endforeach
 </tr>
