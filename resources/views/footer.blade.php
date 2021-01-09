5481f417a4e94260a715fafcef43ad35f93acb8e
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 11:21:51 +0330

    revert back to custom css

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,11 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-        <style>html{-webkit-print-color-adjust:exact}</style>
-    </head>
-	<body>
+<style>
+    div {
+        font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        font-size: 12px;
+        width: 100%;
+    }
+    .flex {
+        display: flex;
+    }
+    .justify-between {
+        justify-content: space-between;
+    }
+    .p-12 {
+        padding: 4rem;
+    }
+    .text-lg {
+        font-size: 2rem;
+    }
+    div {
+        -webkit-print-color-adjust: exact;
+    }
+</style>
 <div class="flex justify-between p-12 text-lg">
     <div class="date"></div>
     <div class="flex">
@@ -14,5 +28,3 @@
         <div class="totalPages"></div>
     </div>
 </div>
-    </body>
-</html>
