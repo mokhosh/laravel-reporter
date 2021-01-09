479f434b56be3d149353e0a05907f7ef7099b1b7
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-09 11:14:36 +0330

    try full html

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,17 +1,12 @@
-<style>
-    .flex {
-        display: flex;
-    }
-    .justify-between {
-        justify-content: space-between;
-    }
-    .p-12 {
-        padding: 4rem;
-    }
-    .text-lg {
-        font-size: 2rem;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+        <title>{{ $title }}</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <style>html{-webkit-print-color-adjust:exact}</style>
+    </head>
+	<body>
 <div class="flex justify-between p-12 text-lg">
     <div class="date"></div>
     <div class="flex">
@@ -20,3 +15,5 @@
         <div class="totalPages"></div>
     </div>
 </div>
+    </body>
+</html>
