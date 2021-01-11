857ab16b566a550f36c6787a4b5c5d7a37836aad
Mohammad Sadeq Khoshnazar <mskhoshnazar@gmail.com>
2021-01-11 10:08:11 +0330

    add basic tests for excel output

---
+++ tests/ExcelTest.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Mokhosh\Reporter\Tests;
+
+
+use Mokhosh\Reporter\Reporter;
+
+class ExcelTest extends BaseTest
+{
+/** @test */
+    public function it_will_send_download_response_with_or_without_download()
+    {
+        User::create([
+            'name' => 'Mo Khosh',
+            'email' => 'mskhoshnazar@gmail.com',
+            'password' => 'password',
+        ]);
+
+        $users = User::query();
+
+        $response = Reporter::report($users)->download()->excel();
+
+        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertStringContainsString('attachment', (string) $response);
+
+        $response = Reporter::report($users, download: true)->excel();
+
+        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertStringContainsString('attachment', (string) $response);
+
+        $response = Reporter::report($users)->excel();
+
+        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertStringContainsString('attachment', (string) $response);
+
+        $response = Reporter::report($users, download: false)->excel();
+
+        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertStringContainsString('attachment', (string) $response);
+    }
+
+    /** @test */
+    public function it_will_name_files_with_correct_extension()
+    {
+        $service = new ExcelService(html: '<div></div>', filename: 'some-file-name');
+
+        $this->assertEquals('some-file-name.xlsx', $service->getFilename());
+    }
+}
